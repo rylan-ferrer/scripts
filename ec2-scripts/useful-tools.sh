5a70ba480578898a5f39fce59f1de173a07879ab
rylan-ferrer <113384111+rylan-ferrer@users.noreply.github.com>
2026-05-27 12:32:35 -0700

    Update useful-tools.sh

--- ec2-scripts/useful-tools.sh
+++ ec2-scripts/useful-tools.sh
@@ -7,24 +7,12 @@ sudo yum update -y
 sudo yum install -y docker 
 sudo usermod -aG docker $USER
 
-#checks docker install
-docker --version
-echo "Docker Installed" 
-
 #installs git
 sudo yum install git -y
-
-#checks git install 
-git --version
-echo "Git Installed"
 
 #installs tree
 sudo yum install tree -y 
 
-#checks tree install 
-tree --version
-echo "Tree Installed"
 
-echo "Setup Complete"
 
 
